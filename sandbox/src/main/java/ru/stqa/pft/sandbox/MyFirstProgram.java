c33df98fcf76f2a79268154bf61934561103432b
SMLogv <s.logvinenko@quality-lab.ru>
2020-11-03 00:26:36 +0300

    3 задание

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,11 +5,11 @@ public class MyFirstProgram {
     public static void main (String[] args) {
         Point p1;
         Point p2;
-        p1 = new Point (0,-10);
-        p2 = new Point(-1, -1);
+        p1 = new Point (0,0);
+        p2 = new Point(2, 0);
         System.out.println(p2.distance(p1));
 
-       /* System.out.println("Диагональ такая =" + p1.distance());  */
     }
 
 }
+
