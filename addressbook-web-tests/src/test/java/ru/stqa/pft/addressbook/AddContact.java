e51deb136caa547c7edee779518a90bb91f3300d
SMLogv <s.logvinenko@quality-lab.ru>
2020-11-15 23:30:37 +0300

    Оптимизировал код создания контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContact.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class AddContact {
+    private final String name;
+    private final String midlename;
+    private final String lastname;
+    private final String nick;
+    private final String address;
+
+    public AddContact(String name, String midlename, String lastname, String nick, String address) {
+        this.name = name;
+        this.midlename = midlename;
+        this.lastname = lastname;
+        this.nick = nick;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMidlename() {
+        return midlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
@@ -1,74 +1,85 @@
 package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AddContactTests {
-  private WebDriver wd;
+    private WebDriver wd;
 
 
-  @BeforeMethod (alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
 
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+        login("admin", "secret");
 
-  }
-
-  @Test
-  public void testAddContact() throws Exception {
-    
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Ivan");
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Pupkin");
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Petriv");
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("IPP");
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Voronezh");
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    wd.findElement(By.linkText("Logout")).click();
-  }
-
-  @AfterMethod (alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    private void login(String name, String password) {
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(name);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
-  }
+
+    @Test
+    public void testAddContact() throws Exception {
+        gotoAddContactPage();
+        fillContactForm(new AddContact("Ivan", "Pupkin", "Petrov", "IPP", "Voronezh"));
+        submitContactCreation();
+    }
+
+    private void submitContactCreation() {
+        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    private void fillContactForm(AddContact addContact) {
+        wd.findElement(By.name("firstname")).sendKeys(addContact.getName());
+        wd.findElement(By.name("middlename")).sendKeys(addContact.getMidlename());
+        wd.findElement(By.name("lastname")).sendKeys(addContact.getLastname());
+        wd.findElement(By.name("nickname")).sendKeys(addContact.getNick());
+        wd.findElement(By.name("address")).sendKeys(addContact.getAddress());
+    }
+
+    private void gotoAddContactPage() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        logout();
+        wd.quit();
+    }
+
+    private void logout() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 
 
 }
