45171d566e118fe4b1d949b418c748c3e9a8c44e
SMLogv <s.logvinenko@quality-lab.ru>
2020-11-21 19:35:19 +0300

    Revert "Оптимизировал тесты с учетом переноса методов в ТестБейс"
    This reverts commit cc36a3d1f025243328d94bd1015df58dee1705ab.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
@@ -1,15 +1,84 @@
 package ru.stqa.pft.addressbook;
 
-import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
-public class AddContactTests extends TestBase {
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class AddContactTests {
+    private WebDriver wd;
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+
+        login("admin", "secret");
+
+    }
+
+    private void login(String name, String password) {
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(name);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
 
     @Test
     public void testAddContact() throws Exception {
         gotoAddContactPage();
         fillContactForm(new AddContact("Ivan", "Pupkin", "Petrov", "IPP", "Voronezh"));
         submitContactCreation();
+    }
+
+    private void submitContactCreation() {
+        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    private void fillContactForm(AddContact addContact) {
+        wd.findElement(By.name("firstname")).sendKeys(addContact.getName());
+        wd.findElement(By.name("middlename")).sendKeys(addContact.getMidlename());
+        wd.findElement(By.name("lastname")).sendKeys(addContact.getLastname());
+        wd.findElement(By.name("nickname")).sendKeys(addContact.getNick());
+        wd.findElement(By.name("address")).sendKeys(addContact.getAddress());
+    }
+
+    private void gotoAddContactPage() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        logout();
+        wd.quit();
+    }
+
+    private void logout() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
 
 
