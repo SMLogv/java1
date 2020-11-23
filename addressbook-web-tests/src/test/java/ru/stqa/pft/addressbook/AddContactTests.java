ee6ff645c167683ac8a9dffa6e5e2cea49a347e5
SMLogv <s.logvinenko@quality-lab.ru>
2020-11-23 22:04:56 +0300

    Создал и заделигировал класс апликейшнМенеджер

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
@@ -7,9 +7,9 @@ public class AddContactTests extends TestBase {
 
     @Test
     public void testAddContact() throws Exception {
-        gotoAddContactPage();
-        fillContactForm(new AddContact("Ivan", "Pupkin", "Petrov", "IPP", "Voronezh"));
-        submitContactCreation();
+        app.gotoAddContactPage();
+        app.fillContactForm(new AddContact("Ivan", "Pupkin", "Petrov", "IPP", "Voronezh"));
+        app.submitContactCreation();
     }
 
 
