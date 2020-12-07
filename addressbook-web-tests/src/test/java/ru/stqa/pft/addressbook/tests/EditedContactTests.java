c5f9a4d0a1c8a434ff7e9a22f9d5f5387f257d83
SMLogv <s.logvinenko@quality-lab.ru>
2020-12-07 21:48:58 +0300

    убрал закомментированные строки в тесте-правке контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/EditedContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/EditedContactTests.java
@@ -5,27 +5,19 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.AddContact;
 import ru.stqa.pft.addressbook.model.TestBase;
 
-import java.time.Duration;
 
 public class EditedContactTests extends  TestBase {
- /*   private  WebDriver wd;
-    private EditedContactTests (WebDriver wd) {
-        this.wd = wd;
-    }  */
+
 
     @Test
     public void testUntitledTestCase() throws InterruptedException {
         app.getNavigationHelper().gotoHomePage1();
         app.getNavigationHelper().clicktoEditContact();
         app.getContactHelper().fillContactForm(new AddContact("Mikle", "Jakson", "ZAra", "IPP", "New York"));
-      /*          wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Jean");
-        wd.findElement(By.xpath("//form[@action='edit.php']")).click();  */
         Thread.sleep(5000);
        app.getNavigationHelper().clicktoSaveUpdateContact();
         app.getNavigationHelper().gotoHomePage2();
-       /*  wd.findElement(By.linkText("Logout")).click();    */
+
     }
 
 
