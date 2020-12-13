876f2528fb6b50d3d0897f484502163643a5e032
SMLogv <s.logvinenko@quality-lab.ru>
2020-12-13 17:27:13 +0300

    Замутил, чтобы тесты в 2-х браузерах гонялись

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,10 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.devtools.v84.browser.Browser;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,21 @@ public class ApplicationManager {
     private  NavigationHelper navigationHelper;
     private  GroupHelper groupHelper;
     private  SessionHelper sessionHelper;
+    private  String browser;
+
+    public ApplicationManager (String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        }
+        else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        }
+
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
