99d3c7da65c25c4d75bef70c84883c45d7d311b9
SMLogv <s.logvinenko@quality-lab.ru>
2021-02-02 21:46:51 +0300

    В ГруппКреэйшн создалпроверки до\после через получение списка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,10 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
 
-import org.testng.*;
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
+
+import java.util.List;
 
 
 public class GroupCreationTests extends TestBase {
@@ -13,10 +15,10 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before +1);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() +1);
     }
 
 
