84e9a60c6058c6d46e843015cf47d184a0d36925
SMLogv <s.logvinenko@quality-lab.ru>
2021-02-06 17:58:08 +0300

    Проверка списка при помощи сравнивателя

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.TestBase;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,7 +29,14 @@ public class GroupCreationTests extends TestBase {
             if (g.getId()>max);
             max=g.getId();
         }
-        group.setId(max);
+        Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+            @Override
+            public int compare(GroupData o1, GroupData o2) {
+                return Integer.compare(o1.getId(), o2.getId());
+            }
+        };
+        int max1=after.stream().max(byId).get().getId();
+        group.setId(max1);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
