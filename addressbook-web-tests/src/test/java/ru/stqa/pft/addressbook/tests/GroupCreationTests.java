7dd69a9fa4518e700f015940fa19320fb2e8201c
SMLogv <s.logvinenko@quality-lab.ru>
2021-02-06 18:10:46 +0300

    Еще уменьшили код сравнения итоговых списковв тесте создания группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -24,13 +24,7 @@ public class GroupCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size() +1);
 
         before.add(group);
-        int max=0;
-        for (GroupData g : after) {
-            if (g.getId()>max);
-            max=g.getId();
-        }
-        int max1=after.stream().max((o1, o2)  ->  Integer.compare(o1.getId(), o2.getId())).get().getId();
-        group.setId(max1);
+        group.setId(after.stream().max((o1, o2)  ->  Integer.compare(o1.getId(), o2.getId())).get().getId());
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
