5ea935153654aaa37ebcacff334b49085024c51d
SMLogv <s.logvinenko@quality-lab.ru>
2021-02-17 09:00:00 +0300

    л5_м6 - подключил Хамскрет, добавил класс групп и отредачил ГруппКреэйшнТест  в лане провеолк в соответствии со флюент-интерфейсом

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 import ru.stqa.pft.addressbook.model.TestBase;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.*;
 
 
 public class GroupCreationTests extends TestBase {
@@ -17,19 +23,18 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() throws Exception {
         app.goTo().groupPage();
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData group =  new GroupData().withName("testARA").withHeader("").withFooter("");
         app.group().create(group);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() +1);
+        Groups after = app.group().all();
+        assertThat(after.size(), equalTo(before.size() +1));
 
 
        // group.setId(after.stream().max((o1, o2)  ->  Integer.compare(o1.getId(), o2.getId())).get().getId());
         // включить, если важны идентификаторы в сранении  + перегенерить иквелс и хешкод в группдата
 
-        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-        before.add(group);
-        Assert.assertEquals(before, after);
 
+        assertThat(after, equalTo(before.withAdded(group
+                .withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 }
