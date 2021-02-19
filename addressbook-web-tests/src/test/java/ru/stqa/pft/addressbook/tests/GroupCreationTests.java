ff0765a7771a99fe5704a5da1a3085b77f33cadc
SMLogv <s.logvinenko@quality-lab.ru>
2021-02-19 09:31:42 +0300

    захешировал проверки в тестах создания/редактирования/удаления групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,17 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 import ru.stqa.pft.addressbook.model.TestBase;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -26,15 +19,23 @@ public class GroupCreationTests extends TestBase {
         Groups before = app.group().all();
         GroupData group =  new GroupData().withName("testARA").withHeader("").withFooter("");
         app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()+1));
         Groups after = app.group().all();
-        assertThat(after.size(), equalTo(before.size() +1));
-
-
        // group.setId(after.stream().max((o1, o2)  ->  Integer.compare(o1.getId(), o2.getId())).get().getId());
         // включить, если важны идентификаторы в сранении  + перегенерить иквелс и хешкод в группдата
-
-
-        assertThat(after, equalTo(before.withAdded(group
+         assertThat(after, equalTo(before.withAdded(group
                 .withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
+
+    @Test
+    public void testBadGroupCreation() throws Exception {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        GroupData group =  new GroupData().withName("tes'").withHeader("").withFooter("");
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before));
+    }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupEditionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupEditionTests.java
@@ -34,8 +34,8 @@ public class GroupEditionTests extends  TestBase {
         GroupData group = new GroupData().
                 withId(modifiedGroup.getId()).withName("Z-Imya").withHeader("Zagolovok").withFooter("Futer");
         app.group().modify(group);
+        assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.group().all();
-        assertEquals(after.size(), before.size());
         assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
     }
 
